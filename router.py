2f23c010a1316a44ab39fcbac10504f602b2b60a
Edwin Wang <lark930@gmail.com>
2017-07-11 11:47:02 +0800

    添加路由：     1，查询到站信息     2，查询线路信息

---
+++ router.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import request
+from bus import Bus
+import json
+
+app = Flask(__name__)
+
+
+@app.route('/bus/<router_name>/stop/<stop_id>')
+def query_stop(router_name, stop_id):
+    direction = request.args.get('direction', '0')
+
+    bus = Bus()
+    res = bus.query_stop(router_name, direction, stop_id)
+
+    return json.dumps(res)
+
+
+@app.route('/bus/<router_name>')
+def query_router(router_name):
+    direction = request.args.get('direction', '0')
+
+    bus = Bus()
+    routers = bus.query_router(router_name, direction)
+    print(routers)
+
+    return json.dumps(routers)
+
